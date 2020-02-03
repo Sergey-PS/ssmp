9568312f90877a2baedfd022acb7456aee36adab
Sergey <pshekus@mail.ru>
2020-02-03 13:27:43 +0600

    control list

--- src/main/java/mn/controller/PersonController.java
+++ src/main/java/mn/controller/PersonController.java
@@ -5,10 +5,10 @@ import mn.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -20,9 +20,9 @@ public class PersonController {
         this.userService = userService;
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<Person> index(@PathVariable int id) {
-        return new ResponseEntity<Person>(userService.get(id), HttpStatus.OK);
+    @GetMapping("")
+    public ResponseEntity<List<Person>> index() {
+        return new ResponseEntity<List<Person>>(userService.getAll(), HttpStatus.OK);
     }
 
 }
--- src/main/java/mn/service/UserService.java
+++ src/main/java/mn/service/UserService.java
@@ -3,20 +3,27 @@ package mn.service;
 import mn.dto.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class UserService {
-    private static final Map<Integer, Person> storage = new HashMap<Integer, Person>();
+    private static final List<Person> storage = new ArrayList<>();
 
     static {
-        storage.put(1, new Person(1,"Petr","Petrov"));
-        storage.put(2, new Person(2,"Ivan","Ivanov"));
-        storage.put(3, new Person(3,"Jack","Sidorov"));
+        storage.add(new Person(1,"Petr","Petrov"));
+        storage.add(new Person(2,"Ivan","Ivanov"));
+        storage.add(new Person(3,"Jack","Sidorov"));
+        storage.add(new Person(4,"Uiiii","jhffsdfkjsdlkfj"));
     }
 
     public Person get(int id) {
         return storage.get(id);
+    }
+
+    public List<Person> getAll() {
+         return storage;
     }
 }
