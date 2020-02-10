54598fde1f8871193b5a243a4c1f0ed6d6d87208
Sergey <pshekus@mail.ru>
2020-02-10 13:55:23 +0600

    add jsonpath

--- src/test/java/mn/PlatformControllerTest.java
+++ src/test/java/mn/PlatformControllerTest.java
@@ -1,5 +1,6 @@
 package mn;
 
+import mn.api.response.PlatformLanguageApi;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +12,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -25,12 +31,19 @@ public class PlatformControllerTest {
 
     @Test
     public void getMessage1() throws Exception {
+        List<PlatformLanguageApi> listLanguage = new ArrayList<>();
+
+        listLanguage.add(new PlatformLanguageApi(1,"Russian"));
+
+
         mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/languages?language=eng").accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.error", Matchers.is("done")))
                 .andExpect(jsonPath("$.total", Matchers.is(3)))
                 .andExpect(jsonPath("$.offset",Matchers.is(0)))
-                .andExpect(jsonPath("$.perPage",Matchers.is(20)));
+                .andExpect(jsonPath("$.perPage",Matchers.is(20)))
+                .andExpect(jsonPath("$.data[0].id",Matchers.is(1)));
     }
+
 
 }
