0a207657d341d001853c70e167dbb2007cac8ff1
Sergey <pshekus@mail.ru>
2020-02-13 14:29:49 +0600

    added all tests

--- src/test/java/mn/PlatformControllerTest.java
+++ src/test/java/mn/PlatformControllerTest.java
@@ -18,12 +18,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PlatformControllerTest {
+
     @Autowired
     private MockMvc mvc;
-
-    public PlatformControllerTest(MockMvc mvc) {
-        this.mvc = mvc;
-    }
 
     @Test
     public void getLanguages1() throws Exception {
@@ -63,4 +60,77 @@ public class PlatformControllerTest {
                 .andExpect(jsonPath("$.data.size()").value(2));
     }
 
+    @Test
+    public void getCities1() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/cities")
+                .accept(MediaType.APPLICATION_JSON_UTF8).param("countryId","1").param("city","o"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.error", Matchers.is("done")))
+                .andExpect(jsonPath("$.total", Matchers.is(4)))
+                .andExpect(jsonPath("$.offset",Matchers.is(0)))
+                .andExpect(jsonPath("$.perPage",Matchers.is(20)))
+                .andExpect(jsonPath("$.data[1].id",Matchers.is(2)));
+    }
+
+    @Test
+    public void getCities2() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/cities")
+                .accept(MediaType.APPLICATION_JSON_UTF8).param("countryId","1").param("city","Mos").param("offset","5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.total", Matchers.is(4)))
+                .andExpect(jsonPath("$.offset",Matchers.is(5)))
+                .andExpect(jsonPath("$.perPage",Matchers.is(20)))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @Test
+    public void getCities3() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/cities")
+                .accept(MediaType.APPLICATION_JSON_UTF8).param("countryId","1").param("city","i").param("itemPerPage","2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.error", Matchers.is("done")))
+                .andExpect(jsonPath("$.total", Matchers.is(4)))
+                .andExpect(jsonPath("$.offset",Matchers.is(0)))
+                .andExpect(jsonPath("$.perPage",Matchers.is(2)))
+                .andExpect(jsonPath("$.data.size()").value(2));
+    }
+
+    @Test
+    public void getCountries1() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/countries")
+                .accept(MediaType.APPLICATION_JSON_UTF8).param("country","an"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.error", Matchers.is("done")))
+                .andExpect(jsonPath("$.total", Matchers.is(4)))
+                .andExpect(jsonPath("$.offset",Matchers.is(0)))
+                .andExpect(jsonPath("$.perPage",Matchers.is(20)))
+                .andExpect(jsonPath("$.data[2].id",Matchers.is(4)));
+    }
+
+    @Test
+    public void getCountries2() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/countries")
+                .accept(MediaType.APPLICATION_JSON_UTF8).param("country","a").param("offset","5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.total", Matchers.is(4)))
+                .andExpect(jsonPath("$.offset",Matchers.is(5)))
+                .andExpect(jsonPath("$.perPage",Matchers.is(20)))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @Test
+    public void getCountries3() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/platform/countries")
+                .accept(MediaType.APPLICATION_JSON_UTF8).param("country","an"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.error", Matchers.is("done")))
+                .andExpect(jsonPath("$.total", Matchers.is(4)))
+                .andExpect(jsonPath("$.offset",Matchers.is(0)))
+                .andExpect(jsonPath("$.perPage",Matchers.is(20)))
+                .andExpect(jsonPath("$.data.size()").value(3));
+    }
 }
